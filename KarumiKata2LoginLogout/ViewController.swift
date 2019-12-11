fe83e73f87e7cbf593485d0b059be7890e709f0e
Pablo Formoso Estrada <pablo@pabloformoso.com>
2019-12-11 15:29:18 +0100

    first

--- KarumiKata2LoginLogout/ViewController.swift
+++ KarumiKata2LoginLogout/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblUser: UITextField!
+    @IBOutlet weak var lblPassword: UITextField!
+    @IBOutlet weak var btnLogOut: UIButton!
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        btnLogOut.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginUser(_ sender: Any) {
+        lblUser.isHidden = true
+        lblPassword.isHidden = true
+        btnLogin.isHidden = true
+        btnLogOut.isHidden = false
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        lblUser.isHidden = false
+        lblPassword.isHidden = false
+        btnLogin.isHidden = false
+        btnLogOut.isHidden = true
+    }
 }
 
