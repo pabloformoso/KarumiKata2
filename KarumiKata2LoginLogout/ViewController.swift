238cffd673f1333ac010f9c1f97cb4fbb51e7fcc
Pablo Formoso Estrada <pablo@pabloformoso.com>
2019-12-11 15:43:58 +0100

    Logic

--- KarumiKata2LoginLogout/ViewController.swift
+++ KarumiKata2LoginLogout/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblPassword: UITextField!
     @IBOutlet weak var btnLogOut: UIButton!
     @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var lblStatus: UILabel!
+    
     
     override func viewWillAppear(_ animated: Bool) {
         btnLogOut.isHidden = true
@@ -24,18 +26,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func loginUser(_ sender: Any) {
-        lblUser.isHidden = true
-        lblPassword.isHidden = true
-        btnLogin.isHidden = true
-        btnLogOut.isHidden = false
+    @IBAction func loginUser(_ sender: Any) {        
+        if (lblUser.text == "admin" && lblPassword.text == "admin") {
+            toogleView()
+        } else {
+            lblStatus.text = "You shall not pass!!"
+        }
     }
     
     @IBAction func logout(_ sender: Any) {
-        lblUser.isHidden = false
-        lblPassword.isHidden = false
-        btnLogin.isHidden = false
-        btnLogOut.isHidden = true
+        let date = Date()
+        let calendar = Calendar.current
+        let seconds = calendar.component(.second, from: date)
+        
+        if (seconds % 2 == 0) {
+            toogleView()
+        }
+    }
+    
+    private func toogleView() {
+        lblUser.isHidden = !lblUser.isHidden
+        lblPassword.isHidden = !lblPassword.isHidden
+        btnLogin.isHidden = !btnLogin.isHidden
+        btnLogOut.isHidden = !btnLogOut.isHidden
+        lblStatus.text = ""
     }
 }
 
